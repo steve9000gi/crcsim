98b8f8c64e8618d7e3c462106f28d6b234bf023b
Steve Chall <stevec@renci.org>
2013-12-04 11:31:51 -0500

    New method setDistanceFilename(String distanceFilename) to call new Geography ctors

--- common/util/src/main/java/org/renci/epi/util/GeographyFactory.java
+++ common/util/src/main/java/org/renci/epi/util/GeographyFactory.java
@@ -2,21 +2,38 @@ package org.renci.epi.util;
 
 /**
  * Initialize geography objects for both statistical models at the outset.
- *   The resultant lookup tables are used in calculations that happen frequently and need to be fast.
+ * The resultant lookup tables are used in calculations that happen frequently
+ * and need to be fast.
  *
  */
 public class GeographyFactory {
     
+    /**
+     * Assign distance filename.
+     * NOTE: This method *must* be called prior to calling getCompliance() and
+     * getModality() in order to instantiate compliance and modality.
+     */
+    public static void setDistanceFilename(String distanceFilename) {
+        if (compliance == null) {
+            compliance 
+                = new Geography (Geography.COMPLIANCE, distanceFilename);
+        }
+        if (modality == null) {
+            modality = new Geography (Geography.MODALITY, distanceFilename);
+        }
+    }
+
     // Compliance model geography
-    private static Geography compliance = new Geography (Geography.COMPLIANCE);
-    
+    private static Geography compliance;
+
     // Modality model geography
-    private static Geography modality = new Geography (Geography.MODALITY);
+    private static Geography modality;
 
     /**
      * Get the geography for the compliance model.
      */
     public static Geography getCompliance () {
+        assert compliance != null;
 	return compliance;
     }
 
@@ -24,6 +41,7 @@ public class GeographyFactory {
      * Get the geography for the modality model.
      */
     public static Geography getModality () {
+        assert modality != null;
 	return modality;
     }
 }
